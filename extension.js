c3f934bda83274148f5d14f1c83d557699ffc8f1
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-11-10 14:02:38 -0500

    add extension.js to prettier ignore

--- extension.js
+++ extension.js
@@ -1,1 +1,9 @@
 const vscode = require("vscode");
+
+/* PLAN --->
+
+jszip
+get Uint8Array with fs.readFile // see docs on web extension limitations
+JSZip.loadAsync(Uint8Array, {options})
+
+<---- END PLAN */
