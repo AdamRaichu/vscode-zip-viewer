529ecedce7ea14fd076f5416e15a0094cd0991e7
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2022-11-12 10:35:54 -0500

    get workspaceFile

--- extension.js
+++ extension.js
@@ -12939,7 +12939,10 @@ vscode.commands.registerCommand("AdamRaichu.zipViewer.extract", function () {
                     if (f.name.endsWith("/")) {
                     } else {
                       f.async("uint8array").then(function (u8) {
-                        vscode.workspace.fs.writeFile(f.name, u8);
+                        vscode.workspace.fs.writeFile(
+                          vscode.workspace.workspaceFile + f.name,
+                          u8
+                        );
                       });
                     }
                   }
