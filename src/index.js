05c007061ad5f9000e431fa346f57c6b987a6980
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2023-12-17 13:33:10 -0800

    use exports.activate in index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import cmds from "./cmds.js";
 import ZipEdit from "./ZipEdit.js";
 import GZipEdit from "./GZipEdit.js";
 
-cmds.register();
-ZipEdit.register();
-GZipEdit.register();
+exports.activate = function(context) {
+  cmds.register();
+  ZipEdit.register();
+  GZipEdit.register();
+}
