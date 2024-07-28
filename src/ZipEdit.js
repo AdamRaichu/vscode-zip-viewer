ef47f5b0de135b29ddf52d357a9332a365cbf6c4
AdamRaichu <91494019+AdamRaichu@users.noreply.github.com>
2024-07-28 14:58:48 -0400

    fix #118
    Thank you @KluSe, and sorry to everyone who was affected by this.
    
    bump package version [rev]

--- src/ZipEdit.js
+++ src/ZipEdit.js
@@ -56,11 +56,10 @@ export default class ZipEdit {
     var customCSS = vscode.workspace.getConfiguration("zipViewer").get("ZipEdit.additionalCSS");
     if (customCSS.includes("</style>")) {
       customCSS = "";
+      vscode.window.showErrorMessage(
+        "Hey! The custom CSS that you are trying to load contains the string `</style>`, which probably means someone is trying to do something nefarious. Don't worry, I prevented it from loading, but please check both your user settings and the workspace settings (zipViewer.ZipEdit.additionalCSS). (If it's in the workspace settings, maybe consider not using this workspace since someone is trying to attack you.)"
+      );
     }
-
-    vscode.window.showErrorMessage(
-      "Hey! The custom CSS that you are trying to load contains the string `</style>`, which probably means someone is trying to do something nefarious. Don't worry, I prevented it from loading, but please check both your user settings and the workspace settings (zipViewer.ZipEdit.additionalCSS). (If it's in the workspace settings, maybe consider not using this workspace since someone is trying to attack you.)"
-    );
 
     panel.webview.options = {
       enableScripts: true,
